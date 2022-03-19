60ac9c3e3929c52354b24ff37759363b5ad90be7
Seth Bonnie <sethkbonnie@gmail.com>
2022-03-19 17:23:42 -0500

    Infer depth from the shape

--- shape/verify_test.go
+++ shape/verify_test.go
@@ -7,47 +7,64 @@ import (
 )
 
 func TestMissing(t *testing.T) {
-	shape := []byte(`
-		README.md
-		LICENSE
-		.github/workflows/
-		go.mod
-	`)
-
 	testCases := []struct {
 		name     string
 		shape    []byte
 		root     string
-		depth    int
 		expected map[string]bool
 	}{
 		{
-			name:     "NoneMissingDefaultDepth",
-			root:     "./testdata",
-			shape:    shape,
-			depth:    0,
+			name: "NoneMissing",
+			root: "./testdata",
+			shape: []byte(`
+				README.md
+				LICENSE
+				.github/workflows/
+				go.mod
+			`),
 			expected: map[string]bool{},
 		},
-		{
-			name:     "NoneMissingHighDepth",
-			root:     "./testdata",
-			shape:    shape,
-			depth:    10,
-			expected: map[string]bool{},
-		},
-		{name: "MissingNestedDir",
-			root:  "./testdata",
-			shape: shape,
-			depth: 1,
+		{name: "MissingDir",
+			root: "./testdata",
+			shape: []byte(`
+				README.md
+				LICENSE
+				go.mod
+				.vscode/
+			`),
 			expected: map[string]bool{
-				".github/workflows": true,
+				".vscode": true,
+			},
+		},
+		{name: "MissingFile",
+			root: "./testdata",
+			shape: []byte(`
+				README.md
+				LICENSE
+				go.mod
+				.env
+			`),
+			expected: map[string]bool{
+				".env": false,
+			},
+		},
+		{name: "MissingNestedFile",
+			root: "./testdata",
+			shape: []byte(`
+				README.md
+				LICENSE
+				go.mod
+				.vscode/settings.json
+			`),
+			expected: map[string]bool{
+				".vscode/settings.json": false,
 			},
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			actual, err := Missing(tc.root, tc.shape, tc.depth)
+			actual, err := Missing(tc.root, tc.shape)
 			assert.Nil(t, err)
 
 			assert.Len(t, actual, len(tc.expected))
